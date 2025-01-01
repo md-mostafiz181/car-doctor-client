992d0137cb1e6aa66f02cb847b0a6062d25897f2
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2025-01-01 10:52:28 +0600

    adde overlay and other functionlity in banner part

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,6 +3,7 @@ import banner2 from "../../assets/images/banner/2.jpg";
 import banner3 from "../../assets/images/banner/3.jpg";
 import banner4 from "../../assets/images/banner/4.jpg";
 import banner5 from "../../assets/images/banner/5.jpg";
+import "../Banner/Banner.css";
 
 const Banner = () => {
   return (
@@ -10,6 +11,29 @@ const Banner = () => {
       <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
           <img src={banner1} className="w-full h-[600px]" />
+          <div className="overlay">
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform ms-28">
+              <div>
+                <h1 className="text-3xl font-bold text-white">
+                  Affordable <br /> Price For Car <br /> Servicing
+                </h1>
+                <p className="text-1xl font-bold text-white mt-2">
+                  There are many variations of passages of available, but the{" "}
+                  <br /> majority have suffered alteration in some form
+                </p>
+
+                <div className="mt-2">
+                  <button className="btn btn-primary me-3">
+                    Discover More
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    Latest Projects
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
             <a href="#slide4" className="btn btn-circle bg-blue-300 mr-2">
               ❮
@@ -18,13 +42,32 @@ const Banner = () => {
               ❯
             </a>
           </div>
-
-          
         </div>
 
-        
         <div id="slide2" className="carousel-item relative w-full">
           <img src={banner2} className="w-full h-[600px]" />
+          <div className="overlay">
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform ms-28">
+              <div>
+                <h1 className="text-3xl font-bold text-white">
+                  Affordable <br /> Price For Car <br /> Servicing
+                </h1>
+                <p className="text-1xl font-bold text-white mt-2">
+                  There are many variations of passages of available, but the{" "}
+                  <br /> majority have suffered alteration in some form
+                </p>
+
+                <div className="mt-2">
+                  <button className="btn btn-primary me-3">
+                    Discover More
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    Latest Projects
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
             <a href="#slide1" className="btn btn-circle bg-blue-300 mr-2">
               ❮
@@ -36,6 +79,28 @@ const Banner = () => {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <img src={banner3} className="w-full h-[600px]" />
+          <div className="overlay">
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform ms-28">
+              <div>
+                <h1 className="text-3xl font-bold text-white">
+                  Affordable <br /> Price For Car <br /> Servicing
+                </h1>
+                <p className="text-1xl font-bold text-white mt-2">
+                  There are many variations of passages of available, but the{" "}
+                  <br /> majority have suffered alteration in some form
+                </p>
+
+                <div className="mt-2">
+                  <button className="btn btn-primary me-3">
+                    Discover More
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    Latest Projects
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
             <a href="#slide2" className="btn btn-circle bg-blue-300 mr-2">
               ❮
@@ -47,6 +112,28 @@ const Banner = () => {
         </div>
         <div id="slide4" className="carousel-item relative w-full">
           <img src={banner4} className="w-full h-[600px]" />
+          <div className="overlay">
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform ms-28">
+              <div>
+                <h1 className="text-3xl font-bold text-white">
+                  Affordable <br /> Price For Car <br /> Servicing
+                </h1>
+                <p className="text-1xl font-bold text-white mt-2">
+                  There are many variations of passages of available, but the{" "}
+                  <br /> majority have suffered alteration in some form
+                </p>
+
+                <div className="mt-2">
+                  <button className="btn btn-primary me-3">
+                    Discover More
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    Latest Projects
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
             <a href="#slide3" className="btn btn-circle bg-blue-300 mr-2">
               ❮
@@ -58,6 +145,28 @@ const Banner = () => {
         </div>
         <div id="slide5" className="carousel-item relative w-full">
           <img src={banner5} className="w-full h-[600px]" />
+          <div className="overlay">
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform ms-28">
+              <div>
+                <h1 className="text-3xl font-bold text-white">
+                  Affordable <br /> Price For Car <br /> Servicing
+                </h1>
+                <p className="text-1xl font-bold text-white mt-2">
+                  There are many variations of passages of available, but the{" "}
+                  <br /> majority have suffered alteration in some form
+                </p>
+
+                <div className="mt-2">
+                  <button className="btn btn-primary me-3">
+                    Discover More
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    Latest Projects
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="absolute  left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end ">
             <a href="#slide4" className="btn btn-circle bg-blue-300 mr-2">
               ❮
