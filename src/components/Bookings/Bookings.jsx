ab9eced3a70ddc9e9b28f600d1d8f4627e9d8e7f
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2025-01-04 10:52:20 +0600

    added delete functionality

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -3,62 +3,72 @@ import { AuthContext } from "../../Providers/AuthProviders";
 import BookingsRow from "./BookingsRow";
 import Container from "../Container/Container";
 
-
 const Bookings = () => {
-    const {user}=useContext(AuthContext)
-    const [bookings,setBookings]=useState([]);
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
 
+  const url = `http://localhost:5000/bookings?email/${user?.email}`;
 
-    const url = `http://localhost:5000/bookings?email/${user?.email}`
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+        console.log(data);
+      });
+  }, []);
 
-    useEffect(()=>{
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>{
-            setBookings(data)
-            console.log(data)
-        })
-    },[])
-    return (
-       <div>
-        <Container>
+  const handleDelete = (id) => {
+    // console.log(id)
+    const proceed = confirm("Are you want to delete?");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("bookings deleted successful.");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+
+  return (
+    <div>
+      <Container>
         <div>
-            <h1>Here I will show of my all bookings:{bookings.length}</h1>
-            <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        {/* <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th> */}
-        <th>Delete</th>
-        <th>Service Image</th>
-        <th>Service Name</th>
-        <th>Price</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-     {
-        bookings.map(booking=> <BookingsRow
-        key={booking._id}
-        booking={booking}
-        ></BookingsRow>)
-     }
- 
- 
-    </tbody>
-
-
-  </table>
-</div>
+          <h1>Here I will show of my all bookings:{bookings.length}</h1>
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr className="bg-blue-300">
+                  <th></th>
+                  <th>Service Image</th>
+                  <th>Service Name</th>
+                  <th>Price</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {bookings.map((booking) => (
+                  <BookingsRow
+                    key={booking._id}
+                    booking={booking}
+                    handleDelete={handleDelete}
+                  ></BookingsRow>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-        </Container>
-       </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Bookings;
--- src/components/Bookings/BookingsRow.jsx
+++ src/components/Bookings/BookingsRow.jsx
@@ -1,43 +1,47 @@
+import { useEffect } from "react";
 import Container from "../Container/Container";
 
+const BookingsRow = ({ booking, handleDelete }) => {
+  const { service_title, img, price, _id } = booking;
 
-const BookingsRow = ({booking}) => {
-    const {service_title,img,price}=booking;
-
-    return (
-
-         <tr>
-        {/* <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th> */}
-        <th>Delete</th>
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle h-12 w-12">
-                <img
-                  src={img}
-                  alt="Avatar Tailwind CSS Component" />
-              </div>
+  return (
+    <tr>
+      <th>
+        <button onClick={() => handleDelete(_id)} className="btn btn-circle bg-blue-300 text-red-600 font-bold">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </th>
+      <td>
+        <div className="flex items-center gap-3">
+          <div className="avatar">
+            <div className="mask mask-squircle h-12 w-12">
+              <img src={img} alt="Avatar Tailwind CSS Component" />
             </div>
-            {/* <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
-            </div> */}
           </div>
-        </td>
-        <td>
-          <p>{service_title}</p>
-        </td>
-        <td>{price}</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
-
-    );
+        </div>
+      </td>
+      <td>
+        <p>{service_title}</p>
+      </td>
+      <td>{price}</td>
+      <th>
+        <button className="btn bg-blue-300 text-orange-600 font-bold btn-xs">Update</button>
+      </th>
+    </tr>
+  );
 };
 
 export default BookingsRow;
