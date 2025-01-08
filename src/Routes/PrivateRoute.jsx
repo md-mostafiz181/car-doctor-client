a5e32225881aaf08968ca99f6a4802b06cb1dcab
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2025-01-08 13:38:52 +0600

    checkout route secure

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProviders";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 
 const PrivateRoute = ({children}) => {
     const {user, loading}=useContext(AuthContext)
+    const location=useLocation()
+    console.log(location.pathname)
     
     if(loading){
         return <progress className="progress w-56"></progress>
